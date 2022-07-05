a35141e84eb66a2e8da9421b66658cf3323127cc
13frfr <2458151214@qq.com>
2022-07-05 14:15:18 +0800

    完成了搜索操作

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,121 @@
 <template>
-  <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <div>
+    <el-input
+      v-model.trim="find"
+      placeholder="搜索-书名名称"
+      style="width: 180px"
+    ></el-input>
+    <button @click="findFn">搜索</button>
+    <el-table :data="tableData" border style="width: 100%">
+      <el-table-column
+        fixed
+        prop="id"
+        label="序号"
+        width="300"
+        v-model="objBook.id"
+      ></el-table-column>
+      <el-table-column
+        prop="bookname"
+        label="书名"
+        width="300"
+        v-model="objBook.bookname"
+      >
+      </el-table-column>
+      <el-table-column
+        prop="author"
+        label="作者"
+        width="200"
+        v-model="objBook.author"
+      ></el-table-column>
+      <el-table-column
+        prop="publisher"
+        label="出版商"
+        width="300"
+        v-model="objBook.publisher"
+      >
+      </el-table-column>
+      <el-table-column label="操作" width="300">
+        <template slot-scope="scope">
+          <el-button @click="handleClick(scope.row)" type="text" size="small"
+            >删除</el-button
+          >
+          <el-button type="text" size="small">详情</el-button>
+        </template>
+      </el-table-column>
+    </el-table>
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
-
 export default {
-  name: 'App',
-  components: {
-    HelloWorld
-  }
+  data() {
+    return {
+      tableData: [],
+      msg:'获取图书列表成功',
+      objBook: {
+        id:'',
+        bookname: '',
+        author: '',
+        publisher: '',
+      },
+      id:'',
+      find: '',
+      bookname: '',
+      author: '',
+      publisher: '',
+    }
+  },
+  methods: {
+    handleClick(row) {
+      console.log(row)
+    },
+    findFn() {
+      if(this.find.length==0) return
+      console.log(this.find);
+      this.$axios({
+          url: 'http://www.liulongbin.top:3006/api/getbooks',
+          method: 'GET',
+          params: {
+            bookname: this.find,
+          },
+        }).then((res)=>{
+          console.log(res.data.data);
+          this.tableData = res.data.data
+          // console.log(this.tableData);
+        })
+      // if (this.find != '') {
+      //   this.$axios({
+      //     url: 'http://www.liulongbin.top:3006/api/getbooks',
+      //     method: 'GET',
+      //     params: {
+      //       bookname: this.find,
+      //     },
+      //   }).then((res)=>{
+      //     console.log(res.data.data);
+      //     this.tableData = res.data.data
+      //     console.log(this.tableData);
+      //   })
+      // }else {
+      //   this.$axios({
+      //     url: "/api/getbooks",
+      //   }).then((res) => {
+      //     console.log(res.data.data);
+      //     this.tableData = res.data.data;
+      //     console.log(this.tableData);
+      //   });
+      // }
+    },
+  },
+  created() {
+    this.$axios({
+      url: 'http://www.liulongbin.top:3006/api/getbooks',
+      method: 'GET', // 默认就是GET方式请求, 可以省略不写
+    }).then((res) => {
+      // console.log(res.data.data)
+      this.tableData = res.data.data
+    })
+    // axios()-原地得到Promise对象
+  },
+  
 }
 </script>
-
-<style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
-</style>
--- src/main.js
+++ src/main.js
@@ -1,6 +1,15 @@
 import Vue from 'vue'
 import App from './App.vue'
 
+import ElementUI from 'element-ui';
+import 'element-ui/lib/theme-chalk/index.css';
+
+import axios from 'axios'
+Vue.prototype.$axios = axios    //全局注册，使用方法为:this.$axios
+
+
+Vue.use(ElementUI);
+
 Vue.config.productionTip = false
 
 new Vue({
