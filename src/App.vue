e0e13b434b4e75f2e0193a328b57e13cfc2c22c4
13frfr <2458151214@qq.com>
2022-07-05 17:04:01 +0800

    完成了图书管理

--- src/App.vue
+++ src/App.vue
@@ -1,48 +1,85 @@
 <template>
   <div>
+    <!-- header -->
     <el-input
       v-model.trim="find"
       placeholder="搜索-书名名称"
       style="width: 180px"
     ></el-input>
+    <!-- body -->
     <button @click="findFn">搜索</button>
-    <el-table :data="tableData" border style="width: 100%">
+    <el-table
+      :data="tableData"
+      @selection-change="handleSelectionChange"
+      border
+      style="width: 100%"
+    >
       <el-table-column
         fixed
         prop="id"
         label="序号"
         width="300"
-        v-model="objBook.id"
       ></el-table-column>
-      <el-table-column
-        prop="bookname"
-        label="书名"
-        width="300"
-        v-model="objBook.bookname"
-      >
+      <el-table-column prop="bookname" label="书名" width="300">
       </el-table-column>
-      <el-table-column
-        prop="author"
-        label="作者"
-        width="200"
-        v-model="objBook.author"
-      ></el-table-column>
-      <el-table-column
-        prop="publisher"
-        label="出版商"
-        width="300"
-        v-model="objBook.publisher"
-      >
+      <el-table-column prop="author" label="作者" width="200"></el-table-column>
+      <el-table-column prop="publisher" label="出版商" width="300">
       </el-table-column>
-      <el-table-column label="操作" width="300">
-        <template slot-scope="scope">
-          <el-button @click="handleClick(scope.row)" type="text" size="small"
-            >删除</el-button
+           <el-table-column prop="address" label="操作" width="%20">
+        <!-- 删除        -->
+        <template slot-scope="scope"
+          ><el-button
+            type="danger"
+            icon="el-icon-delete"
+            style="margin-right: 5px"
+            circle
+            @click="delFn(scope.row.id)"
+          ></el-button>
+                   
+          <el-popover
+            placement="top-start"
+            title="详情"
+            width="200"
+            trigger="hover"
+            :content="`书名:${scope.row.bookname}-作者:${scope.row.author}-出版社:${scope.row.publisher}`"
           >
-          <el-button type="text" size="small">详情</el-button>
+                       
+            <el-button
+              slot="reference"
+              type="primary"
+              icon="el-icon-share"
+              circle
+            ></el-button>
+                     
+          </el-popover>
+                 
         </template>
+             
       </el-table-column>
     </el-table>
+
+    <!-- footer -->
+    <div style="border-top: 1px solid">
+      <input
+        type="text"
+        placeholder="书名"
+        style="width: 20; height: 20px; margin: 15px; border-left: 1px solid"
+        v-model="objBook.bookname"
+      />
+      <input
+        type="text"
+        placeholder="作者"
+        style="width: 20; height: 20px; margin: 15px"
+        v-model="objBook.author"
+      />
+      <input
+        type="text"
+        placeholder="出版社"
+        style="width: 20; height: 20px; margin: 15px"
+        v-model="objBook.publisher"
+      />
+      <el-button type="primary" round @click="addFn">添加</el-button>
+    </div>
   </div>
 </template>
 
@@ -51,14 +88,15 @@ export default {
   data() {
     return {
       tableData: [],
-      msg:'获取图书列表成功',
+      msg: '获取图书列表成功',
       objBook: {
-        id:'',
+        id: '',
         bookname: '',
         author: '',
         publisher: '',
       },
-      id:'',
+      id: '',
+      false: false,
       find: '',
       bookname: '',
       author: '',
@@ -66,44 +104,78 @@ export default {
     }
   },
   methods: {
+    handleSelectionChange(val) {
+      this.multipleSelection = val //拿到选中数据
+      console.log(this.multipleSelection)
+    },
     handleClick(row) {
       console.log(row)
     },
     findFn() {
-      if(this.find.length==0) return
-      console.log(this.find);
+      if (this.find.length == 0) return
+      console.log(this.find)
       this.$axios({
-          url: 'http://www.liulongbin.top:3006/api/getbooks',
-          method: 'GET',
-          params: {
-            bookname: this.find,
-          },
-        }).then((res)=>{
-          console.log(res.data.data);
-          this.tableData = res.data.data
-          // console.log(this.tableData);
+        url: 'http://www.liulongbin.top:3006/api/getbooks',
+        // method: 'GET',
+        params: {
+          bookname: this.find,
+        },
+      }).then((res) => {
+        console.log(res.data.data)
+        this.tableData = res.data.data
+        // console.log(this.tableData);
+      })
+    },
+    addFn() {
+      if (
+        this.objBook.bookname == '' ||
+        this.objBook.author == '' ||
+        this.objBook.publisher == ''
+      )
+        return alert('内容不能为空')
+      this.flag = true
+      this.$axios({
+        url: 'http://www.liulongbin.top:3006/api/addbook',
+        method: 'POST',
+        data: {
+          appkey: '7250d3eb-18e1-41bc-8bb2-11483665535a',
+          ...this.objBook,
+        },
+      }).then((res) => {
+        console.log(res)
+        const id = this.tableData[this.tableData.length - 1]
+          ? this.tableData[this.tableData.length - 1].id + 1
+          : 1
+        this.tableData.push({
+          id,
+          bookname: this.objBook.bookname,
+          author: this.objBook.author,
+          publisher: this.objBook.publisher,
         })
-      // if (this.find != '') {
-      //   this.$axios({
-      //     url: 'http://www.liulongbin.top:3006/api/getbooks',
-      //     method: 'GET',
-      //     params: {
-      //       bookname: this.find,
-      //     },
-      //   }).then((res)=>{
-      //     console.log(res.data.data);
-      //     this.tableData = res.data.data
-      //     console.log(this.tableData);
-      //   })
-      // }else {
-      //   this.$axios({
-      //     url: "/api/getbooks",
-      //   }).then((res) => {
-      //     console.log(res.data.data);
-      //     this.tableData = res.data.data;
-      //     console.log(this.tableData);
-      //   });
-      // }
+        this.objBook.bookname = ''
+        this.objBook.author = ''
+        this.objBook.publisher = ''
+        this.flag = false
+      })
+    },
+    delFn(id) {
+      console.log(id)
+      this.$axios({
+        url: 'http://www.liulongbin.top:3006/api/delbook',
+        method: 'GET',
+        params: {
+          id: id,
+          appkey: '7250d3eb-18e1-41bc-8bb2-11483665535a',
+        },
+      }).then((res) => {
+        this.$axios({
+          url: 'http://www.liulongbin.top:3006/api/getbooks',
+        }).then((res) => {
+          // console.log(res.data.data)
+          this.tableData = res.data.data
+        })
+        // this.findFn()
+      })
     },
   },
   created() {
@@ -116,6 +188,5 @@ export default {
     })
     // axios()-原地得到Promise对象
   },
-  
 }
 </script>
